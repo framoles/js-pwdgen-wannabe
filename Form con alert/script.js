196ce3b3299ab1ac47583e8432883f8b4eaedad0
Francesco Moles <francesco.moles@outlook.it>
2022-03-03 15:59:43 +0100

    end project

---
+++ Form con alert/script.js
@@ -0,0 +1,7 @@
+function getUserInput() {
+    const name = document.getElementById("name").value;
+    const surname = document.getElementById("surname").value;
+    const color = document.getElementById("color").value;
+    const password = name + surname + color + Math.floor(Math.random() * 100);
+    alert("La tua password è:  " + password)
+}
--- script.js
+++ script.js
@@ -1,7 +1,10 @@
-function getUserInput() {
-    const name = document.getElementById("name").value;
-    const surname = document.getElementById("surname").value;
-    const color = document.getElementById("color").value;
-    const password = name + surname + color + Math.floor(Math.random() * 100);
-    document.getElementById("password").innerHTML = password;
-}
+const fname = prompt("Inserire nome");
+const surname = prompt("Inserire cognome");
+const color = prompt("Inserire colore");
+const password = fname + surname + color + Math.floor(Math.random() * 100);
+
+
+document.getElementById("name").innerText = "Nome: " + fname;
+document.getElementById("surname").innerText = "Cognome: " + surname;
+document.getElementById("color").innerText = "Colore: " + color;
+document.getElementById("password").innerText = "La tua password è:  " + password;
